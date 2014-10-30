b8803c604f4836f6f312c0efc9aed6a6c166951f
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-10-30 13:58:12 +0900

    no message

---
+++ Assets/Caustics/Caustics.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Caustics : MonoBehaviour {
+	public const string SHADER_TEXEL_SIZE = "_TexelSize";
+
+	public int n = 512;
+	public Material genMat;
+
+	private Vector4 _texelSize;
+	private RenderTexture[] _causticsTexs;
+	private RenderBuffer[] _colorBuffers;
+	private RenderBuffer _depthBuffer;
+
+	void OnDisable() { Release(); }
+	void Update () {
+		CheckInit();
+
+		genMat.SetVector(SHADER_TEXEL_SIZE, _texelSize);
+		Graphics.SetRenderTarget(_colorBuffers, _depthBuffer);
+		Graphics.Blit(null, genMat, 0);
+		Graphics.SetRenderTarget(null);
+	}
+	
+	void CheckInit() {
+		if (_causticsTexs != null && _causticsTexs.Length > 0 && _causticsTexs[0].width == n)
+			return;
+
+		Release();
+		
+		_texelSize = new Vector4(1f / n, 1f / n, n, n);
+
+		var _causticsTex0 = new RenderTexture(n, n, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+		var _causticsTex1 = new RenderTexture(n, n, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+		_causticsTex0.Create();
+		_causticsTex1.Create();
+
+		_causticsTexs = new RenderTexture[]{ _causticsTex0, _causticsTex1 };
+		_colorBuffers = new RenderBuffer[]{ _causticsTex0.colorBuffer, _causticsTex1.colorBuffer };
+		_depthBuffer = _causticsTex0.depthBuffer;
+	}
+	void Release() {
+		if (_causticsTexs != null) {
+			foreach (var rt in _causticsTexs)
+				rt.Release();
+			_causticsTexs = null;
+		}
+	}
+}
---
+++ Assets/Caustics/CausticsGen.shader
@@ -0,0 +1,50 @@
+﻿Shader "Custom/CausticsGen" {
+	Properties {
+		_BumpMap ("Normal Map", 2D) = "white" {}
+		_Refraction ("Refraction Factor", Float) = 0.7518
+		_Height ("Height", Float) = 0.1
+		_LightDir ("Light Direction", Vector) = (0, 0, 1, 0)
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+	
+			float4 _TexelSize;
+
+			sampler2D _BumpMap;
+			float3 _LightDir;
+
+			struct Input {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			struct vs2ps {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			struct Output {
+				float4 intensity0 : COLOR0;
+				float4 intensity1 : COLOR1;
+			};
+
+			vs2ps vert(Input IN) {
+				vs2ps OUT;
+				OUT.vertex = mul(UNITY_MATRIX_MVP, IN.vertex);
+				OUT.uv = IN.uv;
+				return OUT;
+			}
+			Output frag(vs2ps IN) {
+				Output OUT;
+				
+				return OUT;
+			}
+			ENDCG
+		}
+	} 
+}
